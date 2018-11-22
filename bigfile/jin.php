46c4d943cfbaceaf12affc026267ee20ee239a6e
qiujigoods <44692809+qiujigoods@users.noreply.github.com>
2018-11-22 10:26:52 +0800

    Add files via upload

---
+++ bigfile/jin.php
@@ -0,0 +1,24 @@
+<?php 
+$no=$_POST['fd'];
+echo "<pre>";
+var_dump($no);
+// $name=$_POST['name'];
+// $zong=$_POST['zong'];
+// $file=$_FILES['bolo'];
+// $filename=$file['tmp_name'];
+// $destination='./'.$name.'_'.$no;
+// move_uploaded_file($filename, $destination);
+// if ($no==$zong) {
+// 	for ($i=0; $i <=$zong ; $i++) { 
+// 		$filename='./'.$name.'_'.$i;
+// 		$str.=file_get_contents($filename);
+// 	}
+// 	file_put_contents('./'.$name,$str);
+// 	for ($i=0; $i <=$zong ; $i++) { 
+// 		$filename='./'.$name.'_'.$i;
+// 		unlink($filename);
+// 	}
+// }
+
+
+ ?>
